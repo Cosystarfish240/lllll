50569cc36fee22be1a6da6727286c83739aca245
Cosystarfish240 <72608015+Cosystarfish240@users.noreply.github.com>
2022-01-01 16:54:05 +0530

    Add files via upload

--- main copy 2.js
+++ main copy 2.js
@@ -2,7 +2,7 @@ img = "";
 status = "";
 
 function preload(){
-    img = loadImage('dog_cat.jpg');
+    img = loadImage('L.jpg');
 }
 
 function setup() {
@@ -14,16 +14,10 @@ function setup() {
 function draw(){
     image (img,0,0, 640, 420);
     fill("#FF0000")
-    text("Dog", 45, 75);
+    text("Car", 45, 75);
     noFill();
     stroke("#FF0000");
-    rect(30, 60, 450, 350 );
-
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
+    rect(30, 60, 550, 350);
 }
 function modelLoaded() {
     console.log("Model Loaded!")
